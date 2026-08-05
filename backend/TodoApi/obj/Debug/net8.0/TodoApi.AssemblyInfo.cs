bd3e6fda7a6ea205d47126662c69de6208029fd5
Laís Lara Baptista <lais.lara.baptista@gmail.com>
2026-08-05 19:48:36 -0300

    feat/audit-logout

--- backend/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ backend/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e527d6bbfbf225b126479f05134b4715aef9fc53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+635e4e6f77e2c762f5445d6545666496101f6a05")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
